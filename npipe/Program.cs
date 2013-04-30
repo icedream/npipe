c9d6c75e0535b973123b661af992a83915acc55a
icedream <icedream2k9@die-optimisten.net>
2013-04-30 02:43:09 +0200

    Initial commit

---
+++ npipe/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace npipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() < 2)
+            { Usage(); return; }
+
+            switch (args.First().ToLower())
+            {
+                case "server":
+                    {
+                        try
+                        {
+                            NamedPipeServerStream pipe = new NamedPipeServerStream(args.Last(), PipeDirection.Out, 1);
+                            pipe.WaitForConnection();
+                            using (var stdin = Console.OpenStandardInput())
+                            {
+                                stdin.CopyTo(pipe);
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            return;
+                        }
+                    }
+                    break;
+                case "client":
+                    {
+                        try
+                        {
+                            NamedPipeClientStream pipe = new NamedPipeClientStream(".", args.Last(), PipeDirection.In);
+                            pipe.Connect();
+                            using (var stdout = Console.OpenStandardOutput())
+                            {
+                                pipe.CopyTo(stdout);
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            return;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("\t" + Process.GetCurrentProcess().ProcessName + " <mode> <pipename>");
+            Console.WriteLine();
+            Console.WriteLine("Modes:");
+            Console.WriteLine("\tserver\tMakes data available via a pipe. Writes to pipe from stdin.");
+            Console.WriteLine("\tclient\tMakes data available from a pipe. Reads from pipe to stdout.");
+        }
+    }
+}
---
+++ npipe/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("npipe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Carl Kittelberger")]
+[assembly: AssemblyProduct("npipe")]
+[assembly: AssemblyCopyright("© 2013 Carl Kittelberger")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("dad89c06-6c66-4e29-8ed1-11635c77f54d")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0")]
