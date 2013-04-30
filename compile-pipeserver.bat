e2d201416c6a102a6243e632b79e9948ff140f3c
icedream <icedream2k9@die-optimisten.net>
2013-04-30 02:43:58 +0200

    Remove batch file and ignore pipe.exe generated locally on the server machine.

--- compile-pipeserver.bat
+++
@@ -1,2 +0,0 @@
-"C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\csc.exe" /optimize+ /debug- /nowin32manifest /target:exe /out:npipe.exe npipe\Program.cs npipe\Properties\AssemblyInfo.cs /r:System.Core.dll
-pause
